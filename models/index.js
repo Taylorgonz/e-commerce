759061c503b5d3bae71a07e007da15b5d8da99dd
Taylor Gonzales <hello@taylorgonz.com>
2021-03-22 17:58:52 -0400

    model index js finished

--- models/index.js
+++ models/index.js
@@ -14,9 +14,13 @@ Category.hasMany(Product, {
   foreignKey: 'category_id',
 })
 // Products belongToMany Tags (through ProductTag)
-
+Products.belongsToMany(Tag,  { 
+  through: ProductTag
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: ProductTag
+})
 module.exports = {
   Product,
   Category,
